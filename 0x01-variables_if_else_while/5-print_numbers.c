d93b3265e286b931260b932a09ac11d7a1442854
ItuMilla <itumelenglephondo94@gmail.com>
2023-07-15 11:23:31 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints number base ten
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char base_ten = '0';
+
+	while (base_ten <= '9')
+	{
+		putchar(base_ten);
+		base_ten++;
+	}
+	putchar('\n');
+	return (0);
+}
