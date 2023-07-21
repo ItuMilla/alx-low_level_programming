6e284b6a1731f5b975e6b05bd46016d5d7c3249e
ItuMilla <itumelenglephondo94@gmail.com>
2023-07-21 11:03:01 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if the parameter is a digit or character
+ *
+ * @c: parameter
+ * Return: 1 if its a number and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 &&  c <= 57)
+
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
