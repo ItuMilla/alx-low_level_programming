89cc1fe81f124fdba59a660a9607d7fe03c3458b
ItuMilla <itumelenglephondo94@gmail.com>
2023-07-25 15:12:08 -0700

    A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,10 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+void print_array(int *a, int n);
+int _atoi(char *s);
 
 #endif
