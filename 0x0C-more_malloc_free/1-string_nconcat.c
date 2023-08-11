bd01893489d06b28d8cf50ae8e4ed5e6b483440d
ItuMilla <itumelenglephondo94@gmail.com>
2023-08-11 11:53:29 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,10 +15,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[len1];
-			len1++;
-			while (s2 && s2[len2])
+	while (s1 && s1[len1])
+	{
+		len1++;
+	}
+		while (s2 && s2[len2])
+		{
 			len2++;
+		}
 			if (n < len2)
 			s = malloc(sizeof(char) * (len1 + n + 1));
 			else
@@ -26,8 +30,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			if (!s)
 			return (NULL);
 			while (i < len1)
-			s[i] = s1[i];
-			i++;
+			{
+				s[i] = s1[i];
+				i++;
+			}
 			while (n < len2 && i < (len1 + n))
 			s[i++] = s2[j++];
 			while (n >= len2 && i < (len1 + len2))
