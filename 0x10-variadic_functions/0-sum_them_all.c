b4b221b4efec5fac870cf4eae4a2d5d7a78f25f4
ItuMilla <itumelenglephondo94@gmail.com>
2023-08-17 12:09:59 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,12 +15,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 
 	va_start(ap, n);
-	
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 	
 	va_end(ap);
-
-
 	return (sum);
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,9 +11,8 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *str, *sep = "";
 	va_list list;
-	
-	va_start(list, format);
 
+	va_start(list, format);
 	if (format)
 	{
 		while (format[i])
